76926980149142aedd3f6140c1f454ed79daf4d6
Manjiz <25199230@qq.com>
2018-09-12 11:11:10 +0800

    feat(core): 重构 sessionService（Mark: ES6 Class Problem）
    其中，components/session 又出现了 class 方法丢失的问题

--- packages/olemop/lib/components/session.js
+++ packages/olemop/lib/components/session.js
@@ -1,10 +1,5 @@
-var SessionService = require('../common/service/sessionService');
-
-module.exports = function(app, opts) {
-  var cmp = new Component(app, opts);
-  app.set('sessionService', cmp, true);
-  return cmp;
-};
+const olemopUtils = require('@olemop/utils')
+const SessionService = require('../common/service/sessionService')
 
 /**
  * Session component. Manage sessions.
@@ -12,28 +7,44 @@ module.exports = function(app, opts) {
  * @param {Object} app  current application context
  * @param {Object} opts attach parameters
  */
-var Component = function(app, opts) {
-  opts = opts || {};
-  this.app = app;
-  this.service = new SessionService(opts);
+class Component {
+  constructor (app, opts) {
+    const self = this
+    this.name = '__session__'
+    opts = opts || {}
+    this.app = app
+    this.service = new SessionService(opts)
 
-  var getFun = function(m) {
-    return (function() {
-          return function() {
-            return self.service[m].apply(self.service, arguments);
-          };
-    })();
-  };
-  // proxy the service methods except the lifecycle interfaces of component
-  var method, self = this;
-  for(var m in this.service) {
-    if(m !== 'start' && m !== 'stop') {
-      method = this.service[m];
-      if(typeof method === 'function') {
-        this[m] = getFun(m);
-      }
+    const getFun = (m) => {
+      return (() => {
+        return function () {
+          return self.service[m].apply(self.service, arguments)
+        }
+      })()
     }
+    // proxy the service methods except the lifecycle interfaces of component
+    // var method, self = this
+    // for(var m in this.service) {
+    //   if(m !== 'start' && m !== 'stop') {
+    //     method = this.service[m]
+    //     if(typeof method === 'function') {
+    //       this[m] = getFun(m)
+    //     }
+    //   }
+    // }
+    olemopUtils.listES6ClassMethods(this.service).forEach((field) => {
+      if (field !== 'start' && field !== 'stop') {
+        const method = this.service[field]
+        if (typeof method === 'function') {
+          this[field] = getFun(field)
+        }
+      }
+    })
   }
-};
+}
 
-Component.prototype.name = '__session__';
+module.exports = (app, opts) => {
+  const cmp = new Component(app, opts)
+  app.set('sessionService', cmp, true)
+  return cmp
+}
