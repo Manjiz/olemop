b3bed9e8ba0e91aed5a40a2e16c20a4a7b558bd3
Manjiz <25199230@qq.com>
2018-09-12 14:37:18 +0800

    feat(utils): 加上 invokeCallback 和 size

--- packages/olemop-utils/index.js
+++ packages/olemop-utils/index.js
@@ -2,7 +2,7 @@
  * 列出ES6的一个Class实例上的所有方法，但不包括父类的
  * @param objInstance
  */
-exports.listES6ClassMethods = function (objInstance) {
+exports.listES6ClassMethods = (objInstance) => {
   if (objInstance.prototype && objInstance.prototype.constructor === objInstance) {
     const names = []
     const methodNames = Object.getOwnPropertyNames(objInstance.prototype)
@@ -24,4 +24,24 @@ exports.listES6ClassMethods = function (objInstance) {
     }
     return names
   }
+}
+
+/**
+ * Invoke callback with check
+ * @param {Function} cb 准回调函数
+ */
+exports.invokeCallback = (cb) => {
+  cb && typeof cb === 'function' && cb.apply(null, Array.prototype.slice.call(arguments, 1))
+}
+
+/**
+ * 统计一个对象里属性（非方法）的个数
+ * @param {Object} obj
+ */
+exports.size = (obj) => {
+  let count = 0
+  for (let key in obj) {
+    obj.hasOwnProperty(key) && typeof obj[key] !== 'function' && count++
+  }
+  return count
 }
