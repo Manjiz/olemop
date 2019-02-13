3f06df8ed3093d1d4996a9122a85e5fbbd8a318d
Manjiz <25199230@qq.com>
2019-02-13 19:59:03 +0800

    style(chore): lib/monitor 改写 class

--- packages/olemop/lib/monitor/monitor.js
+++ packages/olemop/lib/monitor/monitor.js
@@ -2,84 +2,82 @@
  * Component for monitor.
  * Load and start monitor client.
  */
-var logger = require('@olemop/logger').getLogger('olemop', __filename)
-var admin = require('@olemop/admin')
-var moduleUtil = require('../util/moduleUtil')
-var utils = require('../util/utils')
-var Constants = require('../util/constants')
 
-var Monitor = function (app, opts) {
-  opts = opts || {}
-  this.app = app
-  this.serverInfo = app.getCurServer()
-  this.masterInfo = app.getMaster()
-  this.modules = []
-  this.closeWatcher = opts.closeWatcher
+const logger = require('@olemop/logger').getLogger('olemop', __filename)
+const admin = require('@olemop/admin')
+const utils = require('../util/utils')
+const moduleUtil = require('../util/moduleUtil')
+const Constants = require('../util/constants')
 
-  this.monitorConsole = admin.createMonitorConsole({
-    id: this.serverInfo.id,
-    type: this.app.getServerType(),
-    host: this.masterInfo.host,
-    port: this.masterInfo.port,
-    info: this.serverInfo,
-    env: this.app.get(Constants.RESERVED.ENV),
-    // auth server function
-    authServer: app.get('adminAuthServerMonitor')
-  })
+class Monitor {
+  constructor(app, opts = {}) {
+    this.app = app
+    this.serverInfo = app.getCurServer()
+    this.masterInfo = app.getMaster()
+    this.modules = []
+    this.closeWatcher = opts.closeWatcher
+
+    this.monitorConsole = admin.createMonitorConsole({
+      id: this.serverInfo.id,
+      type: this.app.getServerType(),
+      host: this.masterInfo.host,
+      port: this.masterInfo.port,
+      info: this.serverInfo,
+      env: this.app.get(Constants.RESERVED.ENV),
+      // auth server function
+      authServer: app.get('adminAuthServerMonitor')
+    })
+  }
+
+  start(cb) {
+    moduleUtil.registerDefaultModules(false, this.app, this.closeWatcher)
+    this.startConsole(cb)
+  }
+
+  startConsole(cb) {
+    moduleUtil.loadModules(this, this.monitorConsole)
+
+    this.monitorConsole.start((err) => {
+      if (err) {
+        utils.invokeCallback(cb, err)
+        return
+      }
+      moduleUtil.startModules(this.modules, (err) => {
+        utils.invokeCallback(cb, err)
+      })
+    })
+
+    this.monitorConsole.on('error', (err) => {
+      if (err) {
+        logger.error('monitorConsole encounters with error: %j', err.stack)
+      }
+    })
+  }
+
+  stop(cb) {
+    this.monitorConsole.stop()
+    this.modules = []
+    process.nextTick(() => {
+      utils.invokeCallback(cb)
+    })
+  }
+
+  // monitor reconnect to master
+  reconnect(masterInfo) {
+    this.stop(() => {
+      this.monitorConsole = admin.createMonitorConsole({
+        id: this.serverInfo.id,
+        type: this.app.getServerType(),
+        host: masterInfo.host,
+        port: masterInfo.port,
+        info: this.serverInfo,
+        env: this.app.get(Constants.RESERVED.ENV)
+      })
+      this.startConsole(() => {
+        logger.info('restart modules for server : %j finish.', self.app.serverId)
+      })
+    })
+  }
 }
 
 module.exports = Monitor
-
-Monitor.prototype.start = function (cb) {
-  moduleUtil.registerDefaultModules(false, this.app, this.closeWatcher)
-  this.startConsole(cb)
-}
-
-Monitor.prototype.startConsole = function (cb) {
-  moduleUtil.loadModules(this, this.monitorConsole)
-
-  var self = this
-  this.monitorConsole.start(function (err) {
-    if (err) {
-      utils.invokeCallback(cb, err)
-      return
-    }
-    moduleUtil.startModules(self.modules, function (err) {
-      utils.invokeCallback(cb, err)
-      return
-    })
-  })
-
-  this.monitorConsole.on('error', function (err) {
-    if (err) {
-      logger.error('monitorConsole encounters with error: %j', err.stack)
-      return
-    }
-  })
-}
-
-Monitor.prototype.stop = function (cb) {
-  this.monitorConsole.stop()
-  this.modules = []
-  process.nextTick(function () {
-    utils.invokeCallback(cb)
-  })
-}
-
-// monitor reconnect to master
-Monitor.prototype.reconnect = function (masterInfo) {
-  var self = this
-  this.stop(function () {
-    self.monitorConsole = admin.createMonitorConsole({
-      id: self.serverInfo.id,
-      type: self.app.getServerType(),
-      host: masterInfo.host,
-      port: masterInfo.port,
-      info: self.serverInfo,
-      env: self.app.get(Constants.RESERVED.ENV)
-    })
-    self.startConsole(function () {
-      logger.info('restart modules for server : %j finish.', self.app.serverId)
-    })
-  })
-}
